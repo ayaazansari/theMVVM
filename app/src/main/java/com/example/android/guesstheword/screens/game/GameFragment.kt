e3dfe9fdb5eeed6aa520fe625bbd38ed13dfe524
ayaaz <ayaazansari3052@gmail.com>
2020-10-19 02:40:14 +0530

    final app commit

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
@@ -21,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -32,8 +17,6 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
 class GameFragment : Fragment() {
 
     private lateinit var viewModel: GameViewModel
-
-
     private lateinit var binding: GameFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -61,18 +44,27 @@ class GameFragment : Fragment() {
             binding.scoreText.text = newScore.toString()
         })
 
+        viewModel.eventGameFinish.observe(this, Observer { hasFinished->
+           if(hasFinished) {
+               gameFinished()
+               viewModel.onGameFinishComplete()
+           }
+        })
         updateWordText()
         return binding.root
 
     }
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?:0)
-        findNavController(this).navigate(action)
+//        val action = GameFragmentDirections.actionGameToScore()
+//        val currentScore =viewModel.score.value?:0
+//        action.se(currentScore)
+////
+//        findNavController(this).navigate(action)
+        Toast.makeText(this.activity ,"Game has finished",Toast.LENGTH_SHORT).show()
     }
 
     private fun updateWordText() {
         binding.wordText.text = viewModel.word
-
     }
 
 
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,29 +1,13 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.guesstheword.screens.game
 
+import android.text.BoringLayout
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
-/**
- * ViewModel containing all the logic needed to run the game
- */
 class GameViewModel : ViewModel() {
 
      var word = ""
@@ -33,6 +17,10 @@ class GameViewModel : ViewModel() {
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish:LiveData<Boolean>
+    get()= _eventGameFinish
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
@@ -75,7 +63,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
+            _eventGameFinish.value = true
         } else {
             word = wordList.removeAt(0)
         }
@@ -88,6 +76,10 @@ class GameViewModel : ViewModel() {
      fun onCorrect() {
          score.value = score.value?.plus(1)
         nextWord()
+    }
+
+    fun onGameFinishComplete(){
+        _eventGameFinish.value = false
     }
 
 }
