791bd251c60b3a2ad31be3193e816772ca12c3f8
ayaaz <ayaazansari3052@gmail.com>
2020-10-19 00:42:19 +0530

    commit that works fine!!

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -25,15 +25,67 @@ import androidx.lifecycle.ViewModel
  */
 class GameViewModel : ViewModel() {
 
-    // TODO (06) Once you've copied over the variables and methods, remove any code referring back
-    // to the GameFragment. You can also clean up the log statements from the last step.
+     var word = ""
+
+    // The current score
+     var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+     fun onSkip() {
+        score--
+        nextWord()
+    }
+
+     fun onCorrect() {
+        score++
+        nextWord()
+    }
+
 }
